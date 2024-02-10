13afc6cf3265b0713134bb1736153b4dfa2ae06d
Magyar-Hunor Tamas <tamasmagyarhunor88@gmail.com>
2024-02-10 22:36:05 +0000

    Finish json challenge

--- extension_challenges/02_json/program/lib/json_load.py
+++ extension_challenges/02_json/program/lib/json_load.py
@@ -1,3 +1,5 @@
+from urllib.request import urlopen
+import json
 # == INSTRUCTIONS ==
 #
 # Below, you'll find lots of incomplete functions.
@@ -14,7 +16,10 @@
 #   Call:    load_data_from_url("https://example.org/my.json")
 #   Returns: A JSON object
 def load_data_from_url(url):
-    pass
+    request = urlopen(url)
+    response = request.read().decode('UTF-8')
+    json_data = json.loads(response)
+    return json_data
 
 # Purpose: Use Python libraries to open the specified file, convert the
 #          data to JSON, and return the data.
@@ -22,7 +27,9 @@ def load_data_from_url(url):
 #   Call:    load_data_from_file("my_test_data.json")
 #   Returns: A JSON object
 def load_data_from_file(filename):
-    pass
+    file = open(f"{filename}")
+    json_data = json.load(file)
+    return json_data
 
 # Purpose: Load the sample JSON from file, and returns a list of films 
 #           directed by the named person.
@@ -30,7 +37,19 @@ def load_data_from_file(filename):
 #   Call:    get_films_by_director("my_test_data.json", "Olivia Wilde")
 #   Returns: ["Booksmart, "Don't Worry Darling"]
 def get_films_by_director(filename, director):
-    pass
+    json_data = load_data_from_file(filename)
+    # json_str = json.dumps(json_data, indent=4)
+    # print(json_str)
+    filtered_list = list(filter(lambda item: item['director'] == director, json_data))
+    movies = []
+    for dictionary in filtered_list:
+        movies.append(dictionary['name'])
+    return movies
+
+    # return list(set(item['name'] for item in json_data if item.get('director') == director))
+
+
+
 
 # Purpose: Load the sample JSON from file, and returns a list of films 
 #           starring the named person.
@@ -38,7 +57,12 @@ def get_films_by_director(filename, director):
 #   Call:    get_films_by_actor("my_test_data.json", "Dwayne Johnson")
 #   Returns: ["Jumanji", "Jungle Cruise"]
 def get_films_by_actor(filename, desired_actor):
-    pass
+    json_data = load_data_from_file(filename)
+    filtered_list = [item for item in json_data if desired_actor in item.get('stars', [])]
+    movies = []
+    for dictionary in filtered_list:
+        movies.append(dictionary['name'])
+    return movies
 
 # Purpose: Load the sample JSON from file, and returns a list of films 
 #           with a rating which is AT LEAST the value specified.
@@ -46,7 +70,12 @@ def get_films_by_actor(filename, desired_actor):
 #   Call:    get_films_with_minimum_rating("test.json", 9.3)
 #   Returns: ["The Shawshank Redemption"]
 def get_films_with_minimum_rating(filename, rating):
-    pass 
+    json_data = load_data_from_file(filename)
+    filtered_list =  list(filter(lambda item: item['imdb_rating'] >= rating, json_data))
+    movies = []
+    for dictionary in filtered_list:
+        movies.append(dictionary['name'])
+    return movies
 
 # Purpose: Load the sample JSON from file, and returns a list of films 
 #           which were released during the specified years.
@@ -54,7 +83,12 @@ def get_films_with_minimum_rating(filename, rating):
 #   Call:    get_films_within_year_range("my_test_data.json", 1994, 1996)
 #   Returns: ["The Lion King", "Independence Day"]
 def get_films_within_year_range(filename, start_year, end_year):
-    pass
+    json_data = load_data_from_file(filename)
+    filtered_list =  list(filter(lambda item: item['year'] >= start_year and item['year'] <= end_year, json_data))
+    movies = []
+    for dictionary in filtered_list:
+        movies.append(dictionary['name'])
+    return movies
 
 # Purpose: Load the sample JSON from file, and returns a list of films 
 #           in order of the year that they were released.
@@ -62,7 +96,12 @@ def get_films_within_year_range(filename, start_year, end_year):
 #   Call:    order_films_chronologically("test.json")
 #   Returns: ["12 Angry Men", "The Godfather", "The Godfather: Part II", ... ]
 def order_films_chronologically(filename):
-    pass
+    json_data = load_data_from_file(filename)
+    sorted_list = sorted(json_data, key = lambda item: item['year'])
+    movies = []
+    for dictionary in sorted_list:
+        movies.append(dictionary['name'])
+    return movies
 
 # Purpose: Load the sample JSON from file, and returns a list of films 
 #           starting with the most recent.
@@ -70,7 +109,12 @@ def order_films_chronologically(filename):
 #   Call:    order_films_most_recent_first("test.json")
 #   Returns: ["The Dark Knight", "The Shawshank Redemption", "The Godfather: Part II", ... ]
 def order_films_most_recent_first(filename):
-    pass
+    json_data = load_data_from_file(filename)
+    sorted_list = sorted(json_data, key = lambda item: item['year'], reverse=True)
+    movies = []
+    for dictionary in sorted_list:
+        movies.append(dictionary['name'])
+    return movies
 
 # Purpose: Load the sample JSON from file, and returns a deduplicated list 
 #           of all the actors whose name begins with that letter,
@@ -79,4 +123,11 @@ def order_films_most_recent_first(filename):
 #   Call:    all_actors_starting_with_letter("test.json", "a")
 #   Returns: ["Aaron Eckhart, "Al Pacino"]
 def all_actors_starting_with_letter(filename, letter):
-    pass
+    json_data = load_data_from_file(filename)
+    filtered_list = [item for item in json_data if any(actor.startswith(letter.upper()) for actor in item.get('stars', []))]
+    actors_set = set()
+    for dictionary in filtered_list:
+        for star in dictionary.get('stars', []):
+            if star[0].lower() == letter.lower():
+                actors_set.add(star)
+    return list(actors_set)
